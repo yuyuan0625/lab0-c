c0d68d5a820e76a683621e174ecda9f633931070
yuyuan0625 <yuyuan0625@gmail.com>
2024-02-28 13:08:10 +0800

    Implement q_free
    The function traverses the linked list using list_for_each_entry_safe
    and frees both the 'value' and the element itself.
    Finally, it frees the head of the linked list.

--- queue.c
+++ queue.c
@@ -23,7 +23,15 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    element_t *current, *safe;
+    list_for_each_entry_safe (current, safe, head, list) {
+        free(current->value);
+        free(current);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
