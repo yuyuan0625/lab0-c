f593382ad9f6e351506b90ec9568178f0b9522a2
yuyuan0625 <yuyuan0625@gmail.com>
2024-03-02 14:08:06 +0800

    Implement q_reverse
    The function utilizes list_move to efficiently reverse the list in
    place.

--- queue.c
+++ queue.c
@@ -174,7 +174,15 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    struct list_head *curr, *safe;
+    list_for_each_safe (curr, safe, head)
+        list_move(curr, head);
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
